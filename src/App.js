f88aa57429b780b2b6c3d71437efea3abb643f48
Azilet <Azilet>
2023-03-10 16:52:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Profile from "./components/header/Profile";
 import user from "./user.json";
 import "./components/header/Profile.css";
+import data from "./data.json";
+import Statistics from "./components/statistics/Statistics";
+import "./components/statistics/statistics.css";
 function App() {
   return (
     <div className="App">
       <Profile {...user} />
+      <Statistics statistics={data} />
     </div>
   );
 }
